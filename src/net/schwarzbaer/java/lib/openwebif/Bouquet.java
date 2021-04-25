11e65ef6bf881e280705090fb34d957cbf06d9b8
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-04-25 17:02:57 +0000

    JAVA: [Eclipse] - Projekt "JavaLib_OpenWebif"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/JavaLib_OpenWebif@5596 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/lib/openwebif/Bouquet.java
+++ src/net/schwarzbaer/java/lib/openwebif/Bouquet.java
@@ -44,4 +44,40 @@ public class Bouquet {
 		return String.format("Bouquet [%d SubServices]", subservices.size());
 	}
 	
-}
+	public SubService getSubService(int i) {
+		if (i<0 || i>=subservices.size()) return null;
+		return subservices.get(i);
+	}
+	
+	public static class SubService {
+
+		public final Service service;
+		final String  servicereference;
+		final Long    program;
+		final String  name;
+		public final Long    pos;
+
+		public SubService(Service service, String servicereference, Long program, String name, Long pos) {
+			this.service = service;
+			this.servicereference = servicereference;
+			this.program = program;
+			this.name = name;
+			this.pos = pos;
+		}
+
+		public static SubService parse(JSON_Object<NV,V> object, BouquetReadInterface bouquetReadInterface) {
+			if (object==null) return null;
+			String servicereference = JSON_Data.getStringValue (object.getValue("servicereference"));
+			Long   program          = JSON_Data.getIntegerValue(object.getValue("program"));
+			String servicename      = JSON_Data.getStringValue (object.getValue("servicename"));
+			Long   pos              = JSON_Data.getIntegerValue(object.getValue("pos"));
+			Service service = null;
+			if (servicereference!=null) service = Service.parseServiceStr(servicereference);
+			if (service==null) return null;
+			
+			if (bouquetReadInterface!=null)
+				bouquetReadInterface.addStationName(service.stationID,servicename);
+			
+			return new SubService(service,servicereference,program,servicename,pos);
+		}
+	}}
