bf97929021cb92aabb338a6c49a5ab6dc2c92174
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-05-12 17:06:26 +0000

    JAVA: [Eclipse] - Projekt "JavaLib_OpenWebif"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/JavaLib_OpenWebif@5667 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/lib/openwebif/EPG.java
+++ src/net/schwarzbaer/java/lib/openwebif/EPG.java
@@ -1,21 +1,28 @@
 package net.schwarzbaer.java.lib.openwebif;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Vector;
 import java.util.function.Consumer;
 
 public class EPG {
 
 	private final Tools tools;
+	private final HashMap<Long,EPGevent> events;
+	private final HashMap<String,StationEPG> stationEPGs;
 	
 	public EPG(Tools tools) {
 		this.tools = tools;
+		events = new HashMap<>();
+		stationEPGs = new HashMap<>();
 	}
 	
 	public interface Tools {
 		String getTimeStr(long millis);
 	}
 	
-	public void getEPGforService(String baseURL, StationID stationID, Long beginTime_UnixTS, Long endTime_UnixTS, Consumer<String> setIndeterminateProgressTask) {
+	public void readEPGforService(String baseURL, StationID stationID, Long beginTime_UnixTS, Long endTime_UnixTS, Consumer<String> setIndeterminateProgressTask) {
 		if (baseURL==null) return;
 		baseURL = OpenWebifTools.removeAllTrailingSlashes(baseURL);
 		String beginTimeStr = beginTime_UnixTS==null ? "" : "&time=" +beginTime_UnixTS.toString();
@@ -37,9 +44,60 @@ public class EPG {
 		
 		addAll(events);
 	}
+	
+	public Vector<EPGevent> getEvents(StationID stationID, long beginTime_UnixTS, long endTime_UnixTS, boolean sorted) {
+		StationEPG stationEPG = stationEPGs.get(stationID.toIDStr(true));
+		if (stationEPG==null) return new Vector<>();
+		return stationEPG.getEvents(events, beginTime_UnixTS, endTime_UnixTS, sorted);
+	}
 
 	private void addAll(Vector<EPGevent> events) {
-		// TODO: store events
+		for (EPGevent event:events) {
+			if (event.id==null) continue;
+			
+			this.events.put(event.id, event);
+			
+			if (event.sref==null) continue;
+			
+			StationEPG stationEPG = stationEPGs.get(event.sref);
+			if (stationEPG==null) stationEPGs.put(event.sref,stationEPG = new StationEPG(event.sref));
+			stationEPG.add(event.id);
+		}
 	}
 	
+	public static class StationEPG {
+
+		private final String sref;
+		private final HashSet<Long> eventIDs;
+
+		public StationEPG(String sref) {
+			this.sref = sref;
+			eventIDs = new HashSet<>();
+		}
+
+		public Vector<EPGevent> getEvents(HashMap<Long,EPGevent> events, long beginTime_UnixTS, long endTime_UnixTS, boolean sorted) {
+			Vector<EPGevent> result = new Vector<>();
+			for (long eventID:eventIDs) {
+				EPGevent event = events.get(eventID);
+				if (!sref.equals(event.sref)) continue;
+				
+				long eventBegin = event.begin_timestamp;
+				long eventDuration = event.duration_sec;
+				if ( eventBegin+eventDuration < beginTime_UnixTS || endTime_UnixTS < eventBegin )
+					continue;
+				
+				result.add(event);
+			}
+			
+			if (sorted)
+				result.sort(Comparator.comparingLong(event->event.begin_timestamp));
+			
+			return result;
+		}
+
+		public void add(long eventID) {
+			eventIDs.add(eventID);
+		}
+		
+	}
 }
